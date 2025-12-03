6eaa40e89b863c9e39f387b3be383b77bd76a119
InfoLearn <infolearn8441@gmail.com>
2025-12-03 17:28:50 +0200

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,23 @@
-const CACHE_NAME = 'infolearn-cache-v1';
-const urlsToCache = [
+const CACHE = 'infolearn-v1';
+const FILES = [
+  './',
   './index.html',
-  './cursos.html',
-  './vagalume.html',
-  './perfil.html',
   './style.css',
-  './courses.js',
-  './utils.js',
+  './manifest.json',
   './icon-192.png',
-  './icon-512.png',
-  './hero-cv.jpg',
-  './facebook.png',
-  './instagram.png',
-  './youtube.png',
-  './linkedin.png',
-  './whatsapp.png'
+  './icon-512.png'
 ];
 
-// Instalando service worker
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+self.addEventListener('install', (e)=> {
+  e.waitUntil(
+    caches.open(CACHE).then(c=> c.addAll(FILES))
   );
 });
 
-// Ativando service worker
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      )
-    )
-  );
+self.addEventListener('activate', (e)=> {
+  e.waitUntil(self.clients.claim());
 });
 
-// Capturando requests
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
