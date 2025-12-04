5876fdeda704ee875e07d59a6baabf7d4f5e3ce7
InfoLearn <infolearn8441@gmail.com>
2025-12-04 18:44:28 +0200

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,26 @@
-const CACHE_NAME = "infolearn-cache-v1";
-const urlsToCache = [
+const cacheName = "infolearn-cache-v1";
+const assetsToCache = [
+  "/",
   "/index.html",
-  "/cursos.html",
-  "/vagalume.html",
-  "/userpanel.html",
   "/style.css",
-  "/main.js",
+  "/firebase.js",
   "/vagalume.js",
   "/userpanel.js",
   "/manifest.json"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(cacheName).then(cache=>{
+      return cache.addAll(assetsToCache);
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(response=>{
+      return response || fetch(e.request);
+    })
   );
 });
