c0c1e1452b3221061f44bd25080254250653ef44
InfoLearn <infolearn8441@gmail.com>
2025-12-04 11:34:45 +0200

    Atualizar o firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,34 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
+// Firebase Core
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    sendPasswordResetEmail
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWM3VAjFNtM4l_UaEg2r0e04sFVJMXudw",
-  authDomain: "infolearn-academy-89bee.firebaseapp.com",
-  projectId: "infolearn-academy-89bee",
-  storageBucket: "infolearn-academy-89bee.firebasestorage.app",
-  messagingSenderId: "65886712812",
-  appId: "1:65886712812:web:0fc810717e1f31b83b86c9"
+    apiKey: "AIzaSyDWM3VAjFNtM4l_UaEg2r0e04sFVJMXudw",
+    authDomain: "infolearn-academy-89bee.firebaseapp.com",
+    projectId: "infolearn-academy-89bee",
+    storageBucket: "infolearn-academy-89bee.firebasestorage.app",
+    messagingSenderId: "65886712812",
+    appId: "1:65886712812:web:0fc810717e1f31b83b86c9"
 };
 
-const app = initializeApp(firebaseConfig);
+// Start Firebase
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+
+// Helpers
+export function registerUser(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function loginUser(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function resetUserPassword(email) {
+    return sendPasswordResetEmail(auth, email);
+}
