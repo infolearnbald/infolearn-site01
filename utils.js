c2fc34de9eb3c10a740902c98525ab2cda223a3a
InfoLearn <infolearn8441@gmail.com>
2025-12-05 16:17:06 +0200

    Delete utils.js

--- utils.js
+++
@@ -1,5 +0,0 @@
-// utils.js - funções utilitárias pequenas
-export function formatDate(ts){
-  try{ return new Date(ts.seconds * 1000).toLocaleString(); }catch(e){ return new Date(ts).toLocaleString(); }
-}
-
