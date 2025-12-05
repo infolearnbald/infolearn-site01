0d1dc8b2ca32a1d3a4c4cde0cdc51b6ed344ec6c
InfoLearn <infolearn8441@gmail.com>
2025-12-05 16:22:04 +0200

    Delete userpanel.js

--- userpanel.js
+++
@@ -1,8 +0,0 @@
-// Área do estudante (cursos gratuitos)
-document.addEventListener('DOMContentLoaded', ()=>{
-  const user = localStorage.getItem('user');
-  if(!user){
-    alert("Faça login primeiro!");
-    window.location.href = "index.html";
-  }
-});
